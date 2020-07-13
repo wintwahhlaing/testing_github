1fa014c9b0189530fa232608751c63b4cc046312
Wint Wah Hlaing <68139841+wintwahhlaing@users.noreply.github.com>
2020-07-12 20:07:12 -0700

    Update email.php

--- email.php
+++ email.php
@@ -2,11 +2,5 @@
 
 echo "sending email";
 echo "another email send";
-echo "third line form online";
-echo "thire line from online";
 
-echo "forth line from online";
-
-
-echo "forth line from cli";
 
